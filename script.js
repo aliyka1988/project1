6c891021d482ea024bb7fe97ab057684dafd3ab6
aliya <aliyka1988@hotmail.com>
2020-12-01 01:17:36 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let group = { 
+    name: "Idiots",
+    age: 15,
+    number: 25
+
+};
+console.log(group.name);
+console.log(group["name"]);
+
+let arr = ['name.png','orange.png','mandarin.png'];
+console.log(arr[1]);
+
+let answer = prompt("Are you 18?", "Yes");
+console.log(typeof(arr));
+
+let incr = 10;
+decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+console.log(5%2);
